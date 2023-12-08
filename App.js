56f832901faa238b30f539445db574e4b6dcdc2c
congit27 <conbo1102a@gmail.com>
2023-12-08 16:35:38 +0700

    Loading fonts

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [fontsLoaded] = useFonts({
+        regular: require('./assets/fonts/Poppins-Regular.ttf'),
+        light: require('./assets/fonts/Poppins-Light.ttf'),
+        bold: require('./assets/fonts/Poppins-Bold.ttf'),
+        medium: require('./assets/fonts/Poppins-Medium.ttf'),
+        semibold: require('./assets/fonts/Poppins-SemiBold.ttf'),
+    });
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.textStyle}> to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textStyle: {
+        fontFamily: 'regular',
+        fontSize: 20,
+    },
 });
